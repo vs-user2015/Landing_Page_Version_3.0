8ec06352c4b0ac882d3cef6cb278a4da04662d8a
kevinmai <kevin.mai@vitalstorm.com>
2015-07-31 11:53:03 -0500

    updated footer includes 07/31

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,1 +1,33 @@
-footer include
+
+					</div>
+				</div>
+			</section>
+			<!-- FOOTER -->
+			<footer id="footer_content">
+				<div class="container">
+					<div class="inner-padding">
+						<div class="contact-us">
+							<p class="title">CALL 254-488-5501</p>
+							<p class="sub-title">Local and Trusted Roofing Experts</p>
+							<!-- BOTTOM NAV -->	
+							<nav class="bottom-nav">
+								<ul>
+									<li><a href="index.php">About Us</a></li>
+									<li><a href="index.php">Our Services</a></li>
+									<li><a href="index.php">Testimonials</a></li>
+									<li><a href="index.php">More...</a></li>
+								</ul>
+							</nav>
+						</div>
+						<div id="copyright">
+							<p>Copyright &copy; Dr. Watts Electric</p>
+							<p>All Rights Reserved</p>
+							<p id="marketing">Marketing by <a title="vitalstorm" href="http://www.vitalstorm.com" target="_blank">Vitalstorm</a></p>
+						</div>
+						<div class="clear"></div>
+					</div>
+				</div>
+			</footer>
+		</section>	
+	</body>
+</html>
