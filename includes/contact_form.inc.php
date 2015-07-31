35a113586641232631b1940b8f5eefc5814d7c0e
sengathit lavanh <sengathit.lavanh@vitalstorm.com>
2015-07-31 11:57:58 -0500

    Includes & contact form
    Cut up files

---
+++ includes/contact_form.inc.php
@@ -0,0 +1,9 @@
+
+<form action="" method="post">
+	<input type="text" name="firstname" value="" class="firstname" placeholder="First Name*">
+	<input type="text" name="phonenumber" value="" class="phonenumber" placeholder="Phone Number*">
+	<input type="text" name="email" value="" class="email" placeholder="Email Address*">
+	<input type="text" name="service-need" value="" class="service-need" placeholder="Service Needed*">
+	<input type="hidden" name="vsref" value="">
+	<input type="submit" value="SUBMIT" id="form_submit">
+</form>	
