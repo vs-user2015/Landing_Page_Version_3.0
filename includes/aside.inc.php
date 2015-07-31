ac149aef61fc42f83c23f54d3390decc07773049
kevinmai <kevin.mai@vitalstorm.com>
2015-07-31 11:49:07 -0500

    updated aside 07/31

--- includes/aside.inc.php
+++ includes/aside.inc.php
@@ -1,1 +1,28 @@
-aside include
+<aside class="side-bar-image-top">
+	<div>
+		<h1>Electrical Services</h1>
+		<nav id="primary_navigation">
+			<ul>
+				<li><a href="index.php">Electrical repair</a></li>
+				<li><a href="index.php">Breaker & Fuse Replacement</a></li>
+				<li><a href="index.php">Ceiling Fans</a></li>
+				<li><a href="index.php">Electrial Panels</a></li>
+				<li><a href="index.php">Surge Protection</a></li>
+				<li><a href="index.php">Home Electrical Inspection</a></li>
+				<li><a href="index.php">Security Lighting</a></li>
+				<li><a href="index.php">Indoor/Outdoor Lighting</a>
+			</ul>	
+		</nav>	
+		<div class="side-bar-image-bottom">
+			<nav id="secondary_navigation">
+				<ul>
+					<li><a href="index.php">About Us</a></li>
+					<li><a href="index.php">Testimonials</a></li>
+					<li><a href="index.php">Current Offers</a></li>
+					<li><a href="index.php">Other Services</a></li>
+					<li><a href="index.php">Coupons</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</aside>
