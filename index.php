71f3eda1066b84c250f471275f3cf6eac23386ea
kevinmai <kevin.mai@vitalstorm.com>
2015-08-05 11:48:59 -0500

    Updated index 0805
    updated h1 h2 index

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php include "includes/header.inc.php" ; ?>
 
 	<div id="copy">
-		<h1>Index Page</h1>
-		<h2>Make the Right Switch</h2>
+		<h1>Make the Right Switch</h1>
+		<h2>Don't get left out in the cold.</h2>
 		<p>When you are in need of an experienced electrician, Charter Electric is the most trusted company for the job. Our expert electricians arrive on time in fully stocked vehicles so that we can respond to your service request quickly and efficiently. Whether your electrical problem is in your home or business, we are ready for your call.</p>
 		<p>Charter Electric delivers quality electrical services according to our good old-fashioned <span class="bold-text">Customer Service Philosophy:</span></p>
 		<p><span class="bold-text">Professionalism</span> - Our polite, uniformed technicians treat your home as if it were their own. We offer scheduled appointment times, constant communication, and upfront pricing.</p>
@@ -11,7 +11,7 @@
 		<p>At Charter Electric, we understand that electrical emergencies are never convenient and sometimes downright dangerous. For this reason, we’re available 24 hours a day, 7 days a week to assist you with any problem that may arise.</p>
 		<p>Because we’re dedicated to providing you with quality service at a price you can trust, you will always be given the cost of the work before we begin. No surprises or hidden fees!</p>
 		<p>We back our work with a 100% customer satisfaction guarantee. If you aren’t satisfied with the work, we will do everything in our power to make it right – at no cost to you. Make the right switch!</p>
-		<p class="bold-text">Your Satisfaction is 100% Guaranteed!</p>
+		<h2>Your Satisfaction is 100% Guaranteed!</h2>
 	</div>
 	<!-- SIDE NAV -->
 	<?php include "includes/aside.inc.php" ; ?>
