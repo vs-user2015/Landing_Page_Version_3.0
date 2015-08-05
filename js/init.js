922d53249c494728cf41fb960b8bbf9f66f9aab2
sengathit lavanh <sengathit.lavanh@vitalstorm.com>
2015-08-05 11:43:02 -0500

    activate stickContact

--- js/init.js
+++ js/init.js
@@ -1,66 +1,68 @@
 jQuery(document).ready(function($){
 	// MAKE CONTACT FORM STICK TO TOP //
-	var $sliding_marque = $("#sliding_marque");
-	var $asset_container = $("#h4_service").parent();
-	var h4_serviceWidth = $("#h4_service").width();
-	var slide_in_numberWidth = $("#slide_in_number").width();
-	var frame_view = $("#h4_service").width();
-	
-	/* Set the position of the sliding of the sliding element #sliding_marque.
-	When #schedule element is fixed to top of the screen the #h4_service element
-	will slide to the left and the phone number will slide into the frame_view 
-	element.  The code below is the initial css style. */
-	$asset_container.css({
-		left : 0,
-		position : "relative",
-		width : h4_serviceWidth + slide_in_numberWidth + 10 + "px",
-		transition : "left 0.3s ease-in-out"
-	});
-	
-	/* Set the #sliding_marque to be the width of the #h4_service and 
-	set and this will be the view port of the #sliding_marque */
-	$sliding_marque.css({ 
-		width : frame_view + "px",
-		overflow : "hidden"
-	});
-	
-	function stickContact(){
-		/* Get the position of the scroll bar */
-		var scrollTop = $(document).scrollTop(); 
+	if($(window).width() >= 768){
+		var $sliding_marque = $("#sliding_marque");
+		var $asset_container = $("#h4_service").parent();
+		var h4_serviceWidth = $("#h4_service").width();
+		var slide_in_numberWidth = $("#slide_in_number").width();
+		var frame_view = $("#h4_service").width();
 		
-		/* Get the height of the #top_container */
-		var topContainerHeight = $("#top_container").outerHeight();
+		/* Set the position of the sliding of the sliding element #sliding_marque.
+		When #schedule element is fixed to top of the screen the #h4_service element
+		will slide to the left and the phone number will slide into the frame_view 
+		element.  The code below is the initial css style. */
+		$asset_container.css({
+			left : 0,
+			position : "relative",
+			width : h4_serviceWidth + slide_in_numberWidth + 10 + "px",
+			transition : "left 0.3s ease-in-out"
+		});
 		
-		/* Get the height of the .slider */
-		var sliderHeight = $(".slider").outerHeight();
+		/* Set the #sliding_marque to be the width of the #h4_service and 
+		set and this will be the view port of the #sliding_marque */
+		$sliding_marque.css({ 
+			width : frame_view + "px",
+			overflow : "hidden"
+		});
 		
-		/* Get handle of the #schedule, this will get fixed to the top if
-		the scrollTop is more than the sum of topContainerHeight and sliderHeight */		
-		var $schedule = $("#schedule");
-		
-		/* Check to see if the scrollTop
-		is more than the sum of topContainerHeight
-		and sliderHeight.  If it is than add a position
-		fixed with top value of 0 for $schedule */
-		if(scrollTop > (topContainerHeight + sliderHeight)){
-			$schedule.css({ 
-				position : "fixed",
-				top : 0,
-			});
-			$asset_container.css({
-				left : (-(h4_serviceWidth)) + "px" 
-			},"fast");
-		}else{
-			$schedule.css( { position: "relative" } );
-			$asset_container.css({
-				left : 0
-			},"fast");
+		function stickContact(){
+			/* Get the position of the scroll bar */
+			var scrollTop = $(document).scrollTop(); 
+			
+			/* Get the height of the #top_container */
+			var topContainerHeight = $("#top_container").outerHeight();
+			
+			/* Get the height of the .slider */
+			var sliderHeight = $(".slider").outerHeight();
+			
+			/* Get handle of the #schedule, this will get fixed to the top if
+			the scrollTop is more than the sum of topContainerHeight and sliderHeight */		
+			var $schedule = $("#schedule");
+			
+			/* Check to see if the scrollTop
+			is more than the sum of topContainerHeight
+			and sliderHeight.  If it is than add a position
+			fixed with top value of 0 for $schedule */
+			if(scrollTop > (topContainerHeight + sliderHeight)){
+				$schedule.css({ 
+					position : "fixed",
+					top : 0,
+				});
+				$asset_container.css({
+					left : (-(h4_serviceWidth)) + "px" 
+				},"fast");
+			}else{
+				$schedule.css( { position: "relative" } );
+				$asset_container.css({
+					left : 0
+				},"fast");
+			}
 		}
+		
+		$(window).scroll(function(){
+			stickContact();
+		});
 	}
-	
-	$(window).scroll(function(){
-		stickContact();
-	});
 	
 	// MOVE #three_box ELEMENT TO FOOTER //
 	if($(window).width() <= 767){
