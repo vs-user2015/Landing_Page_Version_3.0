e379cf497069e8680adb06732e132e87535b2122
vs-user2015 <vs-user2015@users.noreply.github.com>
2015-09-11 15:26:07 -0500

    take dahes out of number_rewrite

--- js/init.js
+++ js/init.js
@@ -1,6 +1,20 @@
 jQuery(document).ready(function($){
-	// GET PHONE NUMBER AND INSERT IT INTO FORM //
 	setTimeout(function(){
+		// Get text node from element
+		var number_rewrite = $("#number_rewrite").html();
+		
+		// Split the number from number_rewrite into arrays 
+		// delimiter is "-"
+		number_rewrite = number_rewrite.split("-");
+		
+		// Turn the number_rewrite arrays into string
+		var uid = number_rewrite.join("");
+		
+		// Get target element to insert number_rewrite into
+		var $vsref = $("input[name='vsref']");
+		
+		// Insert uid into $vsref value attribute
+		$vsref.val(uid);
 		var $number_rewrite = $("#number_rewrite").html();
 		var $vsref = $("input[name='vsref']");
 		$vsref.val($number_rewrite);
